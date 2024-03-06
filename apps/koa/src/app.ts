e409d976911d0d7767470512e5509053c1cee1bd
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-03-06 23:07:20 +0100

    Add Koa (#3)
    * Added koa
    
    * Added fallback route
    
    * Not using http anymore
    
    * Added morgan
    
    * Added health route
    
    ---------
    
    Co-authored-by: Marc Marina <mmarina@findmypast.com>

---
+++ apps/koa/src/app.ts
@@ -0,0 +1,34 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import morgan from 'morgan';
+
+import * as config from './config';
+
+const app = new Koa();
+
+const healthRouter = new Router();
+
+healthRouter.get('/_health', (ctx) => {
+  ctx.body = 'OK';
+});
+
+app.use(healthRouter.routes());
+
+app.use((ctx, next) => {
+  morgan(config.morganFormat)(ctx.req, ctx.res, next);
+});
+
+const router = new Router();
+
+router.get('/', (ctx) => {
+  ctx.body = 'Hello, world!';
+});
+
+app.use(router.routes());
+
+app.use(async (ctx) => {
+  ctx.status = 400;
+  ctx.body = `Cannot ${ctx.method} ${ctx.path}`;
+});
+
+export default app;
---
+++ apps/koa/src/index.ts
@@ -0,0 +1,23 @@
+import app from './app';
+import * as config from './config';
+
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught exception:', err);
+
+  process.exit(1);
+});
+
+const server = app.listen(config.port, () => {
+  console.log(`[${process.pid}] Server listening on port ${config.port}`);
+});
+
+const shutdownHandler = (signal: any) => {
+  console.log(`${signal} received. Closing server.`);
+
+  server.close(() => {
+    process.exit(0);
+  });
+};
+
+process.on('SIGTERM', shutdownHandler);
+process.on('SIGINT', shutdownHandler);
