475f2732eb0623bb2e16114d37c199ac61e32680
Marc Marina <mmarina@findmypast.com>
2025-04-24 15:30:55 +0200

    Shortened class definitions

--- packages/config/src/errors.ts
+++ packages/config/src/errors.ts
@@ -5,38 +5,30 @@ export class ConfigurationError extends Error {
 }
 
 export class MissingEnvironmentVariableError extends ConfigurationError {
-  variableName: string;
-
-  constructor(variableName: string) {
+  constructor(public readonly variableName: string) {
     super(`Missing environment variable: ${variableName}`);
-    this.variableName = variableName;
   }
 }
 
 export class InvalidEnumError extends ConfigurationError {
-  variableName: string;
-  allowedValues: string[];
-  value: string;
-
-  constructor(variableName: string, allowedValues: string[], value: string) {
+  constructor(
+    public readonly variableName: string,
+    public readonly allowedValues: string[],
+    public readonly value: string,
+  ) {
     super(
       `Environment variable ${variableName}=${value} is not one of the allowed values: ${allowedValues.join(', ')}`,
     );
-    this.variableName = variableName;
-    this.allowedValues = allowedValues;
-    this.value = value;
   }
 }
 
 export class InvalidIntegerError extends ConfigurationError {
-  variableName: string;
-  value: string;
-
-  constructor(variableName: string, value: string) {
+  constructor(
+    public readonly variableName: string,
+    public readonly value: string,
+  ) {
     super(
       `Environment variable ${variableName} is not a valid integer: ${value}`,
     );
-    this.variableName = variableName;
-    this.value = value;
   }
 }
