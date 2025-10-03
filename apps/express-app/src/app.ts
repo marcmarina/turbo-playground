407b7fbe0a4abbef35ce6fa8657a9ada79e0f3f0
Marc Marina <mmarina@findmypast.com>
2025-10-03 13:56:38 +0200

    Added metrics and database

--- apps/express-app/src/app.ts
+++ apps/express-app/src/app.ts
@@ -1,9 +1,26 @@
 import express from 'express';
-import packageJson from '../package.json';
+import promBundle from 'express-prom-bundle';
+
 import { getStore, httpContextWrapper } from '@app/context';
 import { httpLogger } from '@app/logger';
 
-const app = express();
+import packageJson from '../package.json';
+import { pool } from './database';
+
+const app = express() as express.Express;
+
+app.use(
+  // @ts-expect-error This is an issue with the types
+  promBundle({
+    formatStatusCode: (res) => {
+      return res.statusCode.toString().charAt(0) + 'xx';
+    },
+    includeMethod: true,
+    includePath: true,
+    includeStatusCode: true,
+    buckets: [0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 15],
+  }),
+);
 
 app.use(express.json());
 
@@ -36,6 +53,16 @@ app.get('/_health', (req, res, next) => {
 
 app.get('/', (req, res) => {
   res.send(packageJson);
+});
+
+app.get('/users', async (req, res, next) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM users');
+
+    res.status(200).json(rows);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use((req, res) => {
--- apps/express-app/src/config.ts
+++ apps/express-app/src/config.ts
@@ -1,3 +1,11 @@
-import { integer, oneOf } from '@app/config';
+import { integer, string } from '@app/config';
 
 export const port = integer('PORT');
+
+export const database = {
+  host: string('DB_HOST'),
+  port: integer('DB_PORT'),
+  database: string('DB_NAME'),
+  user: string('DB_USER'),
+  password: string('DB_PASSWORD'),
+};
