0ea0d8bd46d9acfa8e67600b99cf213034ddf233
Marc Marina <mmarina@findmypast.com>
2025-10-16 16:27:03 +0200

    Revert "Added http agent"
    This reverts commit 8c7cf131147c68b1a3179c19f497ee4429e24449.

--- apps/express-app/src/http-client.ts
+++ apps/express-app/src/http-client.ts
@@ -1,11 +1,6 @@
 import axios from 'axios';
-import http from 'http';
-
 import * as config from './config';
 
 export const httpClient = axios.create({
   baseURL: config.api.host,
-  httpAgent: new http.Agent({
-    keepAlive: false,
-  }),
 });
