316cc52fd24495c343d31ff3425deef0b3a06112
Marc Marina <mmarina@findmypast.com>
2025-11-06 11:26:10 +0800

    Updated /

--- apps/express-app/src/app.ts
+++ apps/express-app/src/app.ts
@@ -66,8 +66,33 @@ app.get('/_health', (req, res, next) => {
   res.send(`OK`);
 });
 
-app.get('/', (req, res) => {
-  res.send(packageJson);
+app.use('/', (req, res) => {
+  res.send(`
+    <html>
+    <head>
+      <title>Express App</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f4f4f4;
+          color: #333;
+          padding: 1rem;
+        }
+      </style>
+    </head>
+    <body>
+    <p>Version: ${packageJson.version}</p>
+    <p>Node.js Version: ${process.version}</p>
+    <p>Hostname: ${os.hostname()}</p>
+    <p>Request ID: ${req.get('x-request-id')}</p>
+    <p>Request Method: ${req.method}</p>
+    <p>Request URL: ${req.url}</p>
+    <p>Request Headers: ${JSON.stringify(req.headers, null, 2)}</p>
+    <p>Request Body: ${JSON.stringify(req.body, null, 2)}</p>
+    <p>Request Query: ${JSON.stringify(req.query, null, 2)}</p>
+    </body>
+    </html>
+`);
 });
 
 app.use((req, res) => {
