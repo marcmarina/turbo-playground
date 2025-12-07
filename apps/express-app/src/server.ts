76431667c16f5907939b9b365312bbf8bbd80be3
Marc Marina <mmarina@findmypast.com>
2025-12-07 23:07:40 +0100

    Added trust proxy

--- apps/express-app/src/server.ts
+++ apps/express-app/src/server.ts
@@ -1,7 +1,7 @@
 import os from 'os';
 import express from 'express';
 import promBundle from 'express-prom-bundle';
-import http from 'http'
+import http from 'http';
 
 import { getStore, httpContextWrapper } from '@app/context';
 import { httpLogger } from '@app/logger';
@@ -11,75 +11,76 @@ import { httpClient } from './http-client';
 import { webSocketManager } from './web-socket-manager';
 
 export function createServer() {
+  const app = express() as express.Express;
 
-const app = express() as express.Express;
+  app.set('trust proxy', true); // or specific IP/CIDR if needed
 
-app.use(
-  // @ts-expect-error This is an issue with the types
-  promBundle({
-    formatStatusCode: (res) => {
-      return res.statusCode.toString().charAt(0) + 'xx';
-    },
-    includeMethod: true,
-    includePath: true,
-    includeStatusCode: true,
-    buckets: [0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 15],
-  }),
-);
+  app.use(
+    // @ts-expect-error This is an issue with the types
+    promBundle({
+      formatStatusCode: (res) => {
+        return res.statusCode.toString().charAt(0) + 'xx';
+      },
+      includeMethod: true,
+      includePath: true,
+      includeStatusCode: true,
+      buckets: [0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 15],
+    }),
+  );
 
-app.use(express.json());
+  app.use(express.json());
 
-app.use((req, res, next) => {
-  httpContextWrapper(next);
-});
-
-app.use((req, res, next) => {
-  const requestId = req.get('x-request-id') ?? crypto.randomUUID();
-
-  req.headers['x-request-id'] = requestId;
-  res.set('x-request-id', requestId);
-
-  const store = getStore();
-
-  store?.set('requestId', requestId);
-
-  next();
-});
-
-app.use(httpLogger);
-
-app.get('/favicon.ico', (req, res) => {
-  return res.status(204).send();
-});
-
-app.get('/get-pod-host', async (req, res, next) => {
-  try {
-    const response = await httpClient.get('/host');
-
-    res.send(response.data);
-  } catch (err) {
-    next(err);
-  }
-});
-
-app.get('/host', (req, res) => {
-  res.send(`Host: ${os.hostname()}`);
-});
-
-app.get('/_health', (req, res, next) => {
-  res.send(`OK`);
-});
-
-app.post('/message', (req, res, next) => {
-  webSocketManager.broadcast(req.body);
-
-  res.send({
-    success: true,
+  app.use((req, res, next) => {
+    httpContextWrapper(next);
   });
-});
 
-app.use('/', (req, res) => {
-  res.send(`
+  app.use((req, res, next) => {
+    const requestId = req.get('x-request-id') ?? crypto.randomUUID();
+
+    req.headers['x-request-id'] = requestId;
+    res.set('x-request-id', requestId);
+
+    const store = getStore();
+
+    store?.set('requestId', requestId);
+
+    next();
+  });
+
+  app.use(httpLogger);
+
+  app.get('/favicon.ico', (req, res) => {
+    return res.status(204).send();
+  });
+
+  app.get('/get-pod-host', async (req, res, next) => {
+    try {
+      const response = await httpClient.get('/host');
+
+      res.send(response.data);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  app.get('/host', (req, res) => {
+    res.send(`Host: ${os.hostname()}`);
+  });
+
+  app.get('/_health', (req, res, next) => {
+    res.send(`OK`);
+  });
+
+  app.post('/message', (req, res, next) => {
+    webSocketManager.broadcast(req.body);
+
+    res.send({
+      success: true,
+    });
+  });
+
+  app.use('/', (req, res) => {
+    res.send(`
     <html>
     <head>
       <title>Express App</title>
@@ -105,16 +106,15 @@ app.use('/', (req, res) => {
     </body>
     </html>
 `);
-});
+  });
 
-app.use((req, res) => {
-  res.status(404).send(`Cannot ${req.method} ${req.url}`);
-});
+  app.use((req, res) => {
+    res.status(404).send(`Cannot ${req.method} ${req.url}`);
+  });
 
-app.use((error, req, res, next) => {
-  res.status(500).send(error.message);
-});
+  app.use((error, req, res, next) => {
+    res.status(500).send(error.message);
+  });
 
-return http.createServer(app)
-
+  return http.createServer(app);
 }
